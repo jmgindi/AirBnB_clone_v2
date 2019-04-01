63efb279b9b1389e237842a45df2fb672ca190c2
BennettDixon <bennettdixon16@gmail.com>
2019-04-01 16:57:37 +0000

    updated models to contain column definitions for sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String, ForeignKey
 
 class State(BaseModel):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
