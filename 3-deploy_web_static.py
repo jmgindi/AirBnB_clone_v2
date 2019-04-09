be8adc6647ab81f46e54a0ad25d7338f3aa78b95
jmgindi <jmgindi@gmail.com>
2019-04-09 20:56:48 +0000

    first commit 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""packs web_static into an archive"""
+from fabric.api import *
+import datetime
+import os
+
+env.hosts = ["35.237.8.203", "34.73.52.180"]
+env.user = "ubuntu"
+
+@runs_once
+def do_pack():
+    """archives the web_static folder
+    """
+    time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    command = local("tar -czvf versions/web_static_{}.tgz web_static"
+                    .format(time))
+    if command.failed:
+        return None
+    else:
+        return command
+
+
+def do_deploy(archive_path):
+    """ deploys web_static.tgz to the web servers
+    """
+    if not archive_path:
+        return False
+
+    status = put(archive_path, "/tmp")
+    if status.failed:
+        return False
+
+    server_path = archive_path.split("/")[-1].split('.')[0]
+    status_2 = run("mkdir -p /data/web_static/releases/{}/"
+                   .format(server_path))
+    if status_2.failed:
+        return False
+
+    run(
+        "tar -xzf /tmp/{}".format(server_path + ".tgz") +
+        " -C /data/web_static/releases/{}/".format(server_path)
+    )
+    run("rm -rf /tmp/{}".format(server_path))
+    run(
+        "mv -n /data/web_static/releases/{}/web_static/* "
+        .format(server_path) +
+        "/data/web_static/releases/{}/".format(server_path)
+    )
+
+    status_3 = run(
+        "rm -rf /data/web_static/releases/{}/web_static"
+        .format(server_path)
+    )
+    if status_3.failed:
+        return False
+
+    status_4 = run("rm -rf /data/web_static/current")
+    if status_4.failed:
+        return False
+
+    status_5 = run(
+        "ln -sf /data/web_static/releases/{}/ "
+        .format(server_path) +
+        "/data/web_static/current"
+    )
+    if status_5.failed:
+        return False
+
+    sudo("sudo service nginx restart")
+
+    return True
+
+def deploy():
+    """packs and deploys web static
+    """
+    tar = do_pack()
+    if tar is None:
+        return False
+    return do_deploy(tar)
