af101725b4510dd78128d1740377da32be9c0487
jmgindi <jmgindi@gmail.com>
2019-04-24 19:24:42 +0000

    first commit files for 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""starts a flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_flask():
+    """returns 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """returns whatever text is after the slash"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is(text="is cool"):
+    """defaults to is cool, same as c_is_fun otherwise"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays n if it is a number"""
+    return "{} is a number".format(n)
+
+
+@app.route("number_template/<int:n>", strict_slashes=False)
+def number_route(n):
+    """displays a template if n is an int"""
+    return render_template("5-number.html", n=n)
+
+if __name__ == "__main__":
+    app.run()
