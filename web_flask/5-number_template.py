9eabdf580e1d5cbda26dd2d91b553d6dcd213e1d
jmgindi <jmgindi@gmail.com>
2019-04-24 19:27:50 +0000

    fixed slash error

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_route(n):
     """displays a template if n is an int"""
     return render_template("5-number.html", n=n)
