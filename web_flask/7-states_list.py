191c7610fc8aed70974eac8274e69f8f6134293d
jmgindi <jmgindi@gmail.com>
2019-04-25 02:05:08 +0000

    added start method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,8 +11,13 @@ def app_teardown(exc=None):
     """calls storage.close() on teardown"""
     storage.close()
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """shows all states"""
     states = [v for k, v in storage.all("State").items()]
     return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run()
